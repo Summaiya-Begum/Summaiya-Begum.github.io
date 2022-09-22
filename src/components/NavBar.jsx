2077f2c8153ce37b8daabe40419f830afc48c69c
SUMMAIYA <103142498+Summaiya-Begum@users.noreply.github.com>
2022-09-23 04:41:27 +0530

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -39,20 +39,16 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
       <div>
-        {/* <h1 className="text-5xl font-signature ml-2"><a className="link-underline hover:transition ease-in-out delay-150 hover:underline hover:decoration-solid" href="https://rahulkarda.netlify.app">Rahul</a></h1> */}
         <Link to="home"><h1 className='myLogo '>Summaiya Begum</h1></Link>
       </div>
 
       <ul
-        style={{
-          color:"aqua"
-          //  textShadow: "0 0 5px #040404, 0 0 15px #4a53cf, 0 0 20px #040404, 0 0 40px #040404, 0 0 60px #3bb6c4, 0 0 10px #4a53cf, 0 0 98px #040404"
-           }}
+        // style={{color:"aqua" }}
         className="hidden lg:flex">
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="nav-links px-4 cursor-pointer capitalize font-medium text-aqua-300 hover:scale-105 hover:text-white duration-200 link-underline"
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-aqua hover:scale-105 hover:text-white duration-200 link-underline"
           >
             <Link to={link} smooth duration={500}>
               {link}
@@ -63,17 +59,17 @@ const NavBar = () => {
 
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500 lg:hidden"
+        className="cursor-pointer pr-4 z-10 text-white lg:hidden "
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-aqua text-black">
           {links.map(({ id, link }) => (
             <li
               key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              className="px-4 cursor-pointer capitalize py-6 text-2xl font-bold"
             >
               <Link
                 onClick={() => setNav(!nav)}
